31ddfd1a12f239f0b24fb0e841fd17ab7db94d94
RaySunWHUT <913283849@qq.com>
2023-06-23 07:16:04 +0800

    更新master部署脚本

--- deploy-master.sh
+++ deploy-master.sh
@@ -2,8 +2,11 @@
 # 捕捉错误
 set -e
 
-printf "=== 执行 $0 脚本 ===\n"
+printf "=== 开始执行 $0 脚本 ===\n"
+printf "本次提交的commt信息为: $1 \n"
 # 发布到github
 git add .
 git commit -m "$1"
 git push -f git@github.com:RaySunWHUT/RaySunWHUT.github.io.git master
+
+printf "=== $0 脚本执行结束 ===\n"
